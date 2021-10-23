9469da70286421fe6dcfd8875a0eb593501c755e
Jishan Kharbanda <66566840+jishk11@users.noreply.github.com>
2021-10-23 11:35:33 -0700

    Create index.js

---
+++ Episode 1 - Basics/index.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js');
+const Client = new Discord.Client();
+
+Client.once('ready', ()=>{
+    console.log('Bot is ready!');
+});
+
+Client.login('ODUwOTg2MTk0MDIzNDgxMzQ1.YLxspw.wD5UjlClmHZh514rjuTyjvqRxg4')
