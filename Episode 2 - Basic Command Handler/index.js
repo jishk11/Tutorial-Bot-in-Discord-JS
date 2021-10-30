ced0b2e070afd6958c5cc137c1ce74dec7b0af9e
Jishan Kharbanda <66566840+jishk11@users.noreply.github.com>
2021-10-29 19:49:51 -0700

    Create index.js

---
+++ Episode 2 - Basic Command Handler/index.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+const Client = new Discord.Client();
+
+const prefix = '!';
+
+
+Client.once('ready', ()=>{
+    console.log('Bot is ready!');
+});
+
+Client.on('message', message =>{
+    if(!message.content.startsWith(prefix)|| message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if (command === 'ping'){ //feel free to change the command names and response to your liking!
+        message.channel.send(`Pong! Latency is **${Date.now() - message.createdTimestamp}ms.**`);
+    } else if (command == 'youtube'){
+        message.channel.send('**Check out Jishan\'s YouTube channel: https://youtube.com/jishankharbanda1**');
+    }
+});
+
+Client.login('YOUR TOKEN HERE')
