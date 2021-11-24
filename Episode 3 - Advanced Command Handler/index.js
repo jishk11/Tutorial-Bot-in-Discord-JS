b255c73895f5214584a7282e0aaaabfb8a3cf5e6
Jishan Kharbanda <66566840+jishk11@users.noreply.github.com>
2021-11-23 20:32:42 -0800

    Create index.js

---
+++ Episode 3 - Advanced Command Handler/index.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+client.commands = new Discord.Collection();
+client.events = new Discord.Collection();
+
+['command_handler', 'event_handler'].forEach(handler =>{
+    require(`./handlers/${handler}`)(client, Discord);
+})      
+
+client.login('YOUR TOKEN HERE');
